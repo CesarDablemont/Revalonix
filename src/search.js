7694d41549f6f762475374041b63e8340f996997
Bapt5962 <bapt5962@gmail.com>
2024-01-14 18:35:53 +0100

    Added button to acess detailled search

---
+++ src/search.js
@@ -0,0 +1,24 @@
+const maxMaxHeightDetailledSearchDivAnim = 60; //In px
+const maxHeightDeltaDetailledSearchDivAnim = 4;
+
+var detailledSearchDiv = document.getElementById("detailled-search");
+var detailledSearchDivShown = false;
+var currentDetailledSearchDivMaxHeight = 0;
+var detailledSearchDivAnim = null;
+
+document.getElementById("detailled-search-button").addEventListener("click", function() {
+    if(detailledSearchDivAnim != null)
+        window.clearInterval(detailledSearchDivAnim);
+    detailledSearchDivShown = !detailledSearchDivShown;
+    detailledSearchDivAnim = setInterval(detailledHideAnim, 16, detailledSearchDivShown);
+});
+
+function detailledHideAnim(shouldShow) {
+    if(shouldShow)
+        currentDetailledSearchDivMaxHeight += maxHeightDeltaDetailledSearchDivAnim;
+    else
+    currentDetailledSearchDivMaxHeight -= maxHeightDeltaDetailledSearchDivAnim;
+    detailledSearchDiv.style["max-height"] = currentDetailledSearchDivMaxHeight + "px";
+    if(currentDetailledSearchDivMaxHeight == 0 || currentDetailledSearchDivMaxHeight == maxMaxHeightDetailledSearchDivAnim)
+        window.clearInterval(detailledSearchDivAnim);
+}
