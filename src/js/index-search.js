b52a5b7092938a1936582db14320cbfdf146ff9c
Bapt5962 <bapt5962@gmail.com>
2024-03-04 17:45:23 +0100

    Clicking on search show search bar

---
+++ src/js/index-search.js
@@ -0,0 +1,10 @@
+var label = document.getElementById('findPostLabel');
+var input = document.getElementById('findPostInput');
+var icon = document.getElementById('findPostIcon');
+
+function searchPostClicked()
+{
+    label.style["display"] = "none";
+    input.style["display"] = "inherit";
+    icon.style["display"] = "inherit";
+}
