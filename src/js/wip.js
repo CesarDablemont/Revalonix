9cb05567a42f6235efc17a041a10dd07ca4c7a60
Bapt5962 <bapt5962@gmail.com>
2024-03-24 15:09:42 +0100

    Added CSS for WIP div

--- src/js/wip.js
+++ src/js/wip.js
@@ -5,6 +5,7 @@ if(isWip)
     var body = document.querySelector("body");
     
     var wipDiv = document.createElement("div");
+    wipDiv.id = "wipDiv";
     
     var wipTxt = document.createElement("p");
     wipTxt.innerHTML = "Le site Internet est encore en construction ! Rien n'est encore définitif et les fonctionnalités ne sont pas encore toutes activées.";
