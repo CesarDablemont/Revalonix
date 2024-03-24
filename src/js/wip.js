36a9cf9a60b8f3530345fec78b2f52ea22cc3ce2
Bapt5962 <bapt5962@gmail.com>
2024-03-24 15:04:51 +0100

    wip.js now generate a message

--- src/js/wip.js
+++ src/js/wip.js
@@ -2,5 +2,13 @@ const isWip = true;
 
 if(isWip)
 {
-    //Do stuff here
+    var body = document.querySelector("body");
+    
+    var wipDiv = document.createElement("div");
+    
+    var wipTxt = document.createElement("p");
+    wipTxt.innerHTML = "Le site Internet est encore en construction ! Rien n'est encore définitif et les fonctionnalités ne sont pas encore toutes activées.";
+    wipDiv.appendChild(wipTxt);
+
+    body.appendChild(wipDiv);
 }
